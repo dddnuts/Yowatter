f1764e615b89d2177c3d6cc31cbb33d2e99faa6a
dddnuts <info@dddnuts.net>
2015-07-26 09:53:53 +0900

    Mod launch screen

---
+++ Yowatter/Source/Models/Color/Color.swift
@@ -0,0 +1,36 @@
+//
+//  Color.swift
+//  Yowatter
+//
+//  Created by t-matsumura on 7/26/15.
+//  Copyright (c) 2015 dddnuts. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    
+    static func colorWithRGB256(red: NSInteger, green: NSInteger, blue: NSInteger) -> UIColor {
+        return UIColor(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    static func darkBlue() -> UIColor {
+        return colorWithRGB256(0, green: 38, blue: 53)
+    }
+    
+    static func blue() -> UIColor {
+        return colorWithRGB256(1, green: 52, blue: 64)
+    }
+    
+    static func red() -> UIColor {
+        return colorWithRGB256(171, green: 26, blue: 37)
+    }
+    
+    static func orange() -> UIColor {
+        return colorWithRGB256(217, green: 121, blue: 37)
+    }
+    
+    static func cream() -> UIColor {
+        return colorWithRGB256(239, green: 231, blue: 190)
+    }
+}
