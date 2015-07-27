cb6c7f043ea855353fbd8bef65edb25db0192e13
dddnuts <info@dddnuts.net>
2015-07-27 12:04:51 +0900

    Fix build target

--- Travis/Scripts/build_ipa.sh
+++ Travis/Scripts/build_ipa.sh
@@ -8,5 +8,5 @@ exit 0
 fi
 
 rm -rf ./build/*
-xcodebuild -workspace ${APP_NAME}.xcworkspace -scheme ${APP_NAME} -sdk iphoneos -configuration Release CODE_SIGN_IDENTITY="${DEVELOPER_NAME}" archive -archivePath ./build/${APP_NAME}.xcarchive
+xcodebuild -project ${APP_NAME}.xcodeproj -scheme ${APP_NAME} -sdk iphoneos -configuration Release CODE_SIGN_IDENTITY="${DEVELOPER_NAME}" archive -archivePath ./build/${APP_NAME}.xcarchive
 xcodebuild -exportArchive -exportFormat IPA -archivePath ./build/${APP_NAME}.xcarchive -exportPath ./build/${APP_NAME}.ipa -exportProvisioningProfile "${PROFILE_NAME}"
